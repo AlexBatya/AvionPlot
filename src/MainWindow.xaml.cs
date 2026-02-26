1ba837d54b8ee4dbe4016979082c98cfc0b22e91
Алексей Пащенко <alex28.12.12@mail.ru>
2026-02-26 13:33:44 +1000

    'new'

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -13,17 +13,17 @@ namespace AvionPlot.Views
     public partial class MainWindow : Window
     {
         private PlotModel plotModel;
-        private List<LineSeries> seriesList = new List<LineSeries>();
+        private List<LineSeries> seriesList;
         private List<RowData> data = new List<RowData>();
+        private GraphMode currentMode = GraphMode.Normal;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            // Инициализация графика
             plotModel = new PlotModel { Title = "Графики осей проезда" };
+            seriesList = new List<LineSeries>();
 
-            // Настройка осей с сеткой
             plotModel.Axes.Add(new LinearAxis
             {
                 Position = AxisPosition.Bottom,
@@ -39,117 +39,14 @@ namespace AvionPlot.Views
                 MinorGridlineStyle = LineStyle.Dot
             });
 
-            // Контроллер для масштабирования и панорамирования
-            var controller = new PlotController();
-            controller.UnbindAll();
-            controller.BindMouseWheel(PlotCommands.ZoomWheel);
-            controller.BindMouseDown(OxyMouseButton.Left, PlotCommands.PanAt);
-            controller.BindMouseDown(OxyMouseButton.Right, PlotCommands.ZoomRectangle);
-            PlotView.Controller = controller;
             PlotView.Model = plotModel;
 
-            // Подключаем кнопку "Открыть"
-            if (MenuBarControl.MainMenu.FindName("OpenMenuItem") is MenuItem openItem)
-                openItem.Click += OpenFile_Click;
-
-            // Drag & Drop
+            // Drag-and-drop
             this.AllowDrop = true;
-            this.DragOver += MainWindow_DragOver;
             this.Drop += MainWindow_Drop;
         }
 
-        #region Drag & Drop
-
-        private void MainWindow_DragOver(object sender, DragEventArgs e)
-        {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop))
-            {
-                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
-                e.Effects = (files.Length > 0 && files[0].EndsWith(".xml", StringComparison.OrdinalIgnoreCase))
-                    ? DragDropEffects.Copy
-                    : DragDropEffects.None;
-            }
-            else e.Effects = DragDropEffects.None;
-
-            e.Handled = true;
-        }
-
-        private void MainWindow_Drop(object sender, DragEventArgs e)
-        {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop))
-            {
-                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
-                if (files.Length > 0 && files[0].EndsWith(".xml", StringComparison.OrdinalIgnoreCase))
-                {
-                    LoadFile(files[0]);
-                }
-            }
-        }
-
-        #endregion
-
-        #region Open File
-
-        private void OpenFile_Click(object sender, RoutedEventArgs e)
-        {
-            var dlg = new OpenFileDialog
-            {
-                Filter = "XML файлы (*.xml)|*.xml",
-                Title = "Выберите файл проезда"
-            };
-
-            if (dlg.ShowDialog() == true)
-            {
-                LoadFile(dlg.FileName);
-            }
-        }
-
-        #endregion
-
-        private void LoadFile(string path)
-        {
-            try
-            {
-                data = DataLoader.LoadFromXml(path);
-
-                // Определяем текущий выбранный режим графика
-                GraphMode mode = GetCurrentGraphMode();
-
-                BuildSeries(mode);
-
-                // Создаём чекбоксы или обновляем существующие
-                if (CheckBoxPanel.Children.Count == 0)
-                    CreateCheckBoxes();
-                else
-                    ApplyCheckBoxStates();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Ошибка при загрузке файла:\n{ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
-
         private enum GraphMode { Normal, Derivative, Acceleration }
-
-        private GraphMode GetCurrentGraphMode()
-        {
-            foreach (var child in LogicalTreeHelper.GetChildren(this))
-            {
-                if (child is StackPanel panel)
-                {
-                    foreach (var rbObj in panel.Children)
-                    {
-                        if (rbObj is RadioButton rb && rb.IsChecked == true)
-                        {
-                            if (rb.Content.ToString() == "Обычный") return GraphMode.Normal;
-                            if (rb.Content.ToString() == "Производная") return GraphMode.Derivative;
-                            if (rb.Content.ToString() == "Ускорение") return GraphMode.Acceleration;
-                        }
-                    }
-                }
-            }
-            return GraphMode.Normal;
-        }
 
         private void BuildSeries(GraphMode mode)
         {
@@ -166,13 +63,20 @@ namespace AvionPlot.Views
             AddSeries(data, d => d.WES910, "WES910", mode);
             AddSeries(data, d => d.WES1112, "WES1112", mode);
 
-            // Применяем текущие состояния чекбоксов
-            ApplyCheckBoxStates();
+            // Применяем состояние чекбоксов
+            foreach (CheckBox cb in CheckBoxPanel.Children)
+            {
+                foreach (var series in seriesList)
+                {
+                    if (series.Title == cb.Content.ToString())
+                        series.IsVisible = cb.IsChecked == true;
+                }
+            }
 
             plotModel.InvalidatePlot(true);
         }
 
-        private void AddSeries(List<RowData> data, Func<RowData, int> selector, string title, GraphMode mode)
+        private void AddSeries(List<RowData> dataList, Func<RowData, int> selector, string title, GraphMode mode)
         {
             var series = new LineSeries
             {
@@ -181,16 +85,17 @@ namespace AvionPlot.Views
             };
 
             List<double> values = new List<double>();
-            foreach (var row in data) values.Add(selector(row));
+            foreach (var row in dataList)
+                values.Add(selector(row));
 
-            if (mode == GraphMode.Derivative && values.Count > 1)
+            if (mode == GraphMode.Derivative)
             {
                 var deriv = new List<double>();
                 for (int i = 1; i < values.Count; i++)
                     deriv.Add(values[i] - values[i - 1]);
                 values = deriv;
             }
-            else if (mode == GraphMode.Acceleration && values.Count > 2)
+            else if (mode == GraphMode.Acceleration)
             {
                 var acc = new List<double>();
                 for (int i = 2; i < values.Count; i++)
@@ -205,45 +110,19 @@ namespace AvionPlot.Views
             plotModel.Series.Add(series);
         }
 
-        private void CreateCheckBoxes()
-        {
-            CheckBoxPanel.Children.Clear();
-            foreach (var series in seriesList)
-            {
-                var cb = new CheckBox
-                {
-                    Content = series.Title,
-                    IsChecked = true,
-                    Margin = new Thickness(5)
-                };
-                cb.Checked += CheckBoxChanged;
-                cb.Unchecked += CheckBoxChanged;
-                CheckBoxPanel.Children.Add(cb);
-            }
-        }
-
-        private void ApplyCheckBoxStates()
-        {
-            foreach (CheckBox cb in CheckBoxPanel.Children)
-            {
-                foreach (var series in seriesList)
-                {
-                    if (series.Title == cb.Content.ToString())
-                        series.IsVisible = cb.IsChecked == true;
-                }
-            }
-        }
-
         private void GraphTypeChanged(object sender, RoutedEventArgs e)
         {
             if (!(sender is RadioButton rb) || rb.IsChecked != true) return;
 
-            GraphMode mode = GraphMode.Normal;
-            if (rb.Content.ToString() == "Обычный") mode = GraphMode.Normal;
-            else if (rb.Content.ToString() == "Производная") mode = GraphMode.Derivative;
-            else if (rb.Content.ToString() == "Ускорение") mode = GraphMode.Acceleration;
+            currentMode = rb.Content.ToString() switch
+            {
+                "Обычный" => GraphMode.Normal,
+                "Производная" => GraphMode.Derivative,
+                "Ускорение" => GraphMode.Acceleration,
+                _ => GraphMode.Normal
+            };
 
-            BuildSeries(mode);
+            BuildSeries(currentMode);
         }
 
         private void CheckBoxChanged(object sender, RoutedEventArgs e)
@@ -260,6 +139,53 @@ namespace AvionPlot.Views
             }
 
             plotModel.InvalidatePlot(true);
+        }
+
+        private void MenuOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new OpenFileDialog { Filter = "XML Files (*.xml)|*.xml" };
+            if (dlg.ShowDialog() == true)
+                LoadFile(dlg.FileName);
+        }
+
+        private void MainWindow_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                if (files.Length > 0)
+                    LoadFile(files[0]);
+            }
+        }
+
+        private void LoadFile(string path)
+        {
+            data = DataLoader.LoadFromXml(path);
+
+            // Сохраняем состояние чекбоксов
+            var checkStates = new Dictionary<string, bool>();
+            foreach (CheckBox cb in CheckBoxPanel.Children)
+            {
+                if (cb.Content != null)
+                    checkStates[cb.Content.ToString()] = cb.IsChecked == true;
+            }
+
+            // Очищаем панель и создаём новые чекбоксы
+            CheckBoxPanel.Children.Clear();
+            foreach (var col in new[] { "OSWES", "WES12", "WES34", "WES56", "WES78", "WES910", "WES1112" })
+            {
+                var cb = new CheckBox
+                {
+                    Content = col,
+                    IsChecked = checkStates.TryGetValue(col, out bool val) ? val : true,
+                    Margin = new Thickness(5)
+                };
+                cb.Checked += CheckBoxChanged;
+                cb.Unchecked += CheckBoxChanged;
+                CheckBoxPanel.Children.Add(cb);
+            }
+
+            BuildSeries(currentMode);
         }
     }
 }
